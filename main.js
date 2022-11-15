fa1898e2401c2cfff56437161d3be160762c0bc5
GrantT7 <117118045+GrantT7@users.noreply.github.com>
2022-11-14 22:11:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,36 @@ const sectionOneObserver = new IntersectionObserver(function(entries, sectionOne
 }, sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+
+// This displays the success/fail message after a form submission
+function message(){
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const phone = dicument.getElementById('phone');
+  const subject = document.getElementById('subject');
+  const msg = document.getElementById('msg');
+  const success = document.getElementById('success');
+  const danger = document.getElementById('danger');
+
+  if(name.value === '' || email.value === '' || phone === '' || subject === '' || msg.value === ''){
+      danger.style.display = 'block';
+  } else {
+      setTimeout(() => {
+          name.value = '';
+          email.value = '';
+          phone.value = '';
+          subject.value = '';
+          msg.value = '';
+      }, 2000);
+
+      success.style.display = 'block';
+  }
+
+
+  setTimeout(() => {
+      danger.style.display = 'none';
+      success.style.display = 'none';
+  }, 4000);
+
+}
